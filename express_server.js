76b5740e66ce2f1e431d6b9fcb5e4b4851afab15
Estella Song <estellajaysong@gmail.com>
2019-03-12 20:52:33 +0000

    added route to urls.index for a list of urls and their shortened form

--- express_server.js
+++ express_server.js
@@ -12,6 +12,10 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase, test: "kkkokl" };
+    res.render("urls_index", templateVars);
+  });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
