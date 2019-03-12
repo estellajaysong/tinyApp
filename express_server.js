fb6522302e07b3511a28bb96afe93707cc1d596c
Estella Song <estellajaysong@gmail.com>
2019-03-12 23:24:05 +0000

    installed and added code to use body-parser

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs") // EJS as templating engine
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -16,6 +18,9 @@ app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
@@ -26,6 +31,7 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
