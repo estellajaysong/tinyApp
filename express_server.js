c923e9cd738b81e8a09485ff987b90a7fb222990
Estella Song <estellajaysong@gmail.com>
2019-03-13 00:22:12 +0000

    completed URL Shortening (Part 2) Assignment, added redirect to post to urls

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   });
+app.get("/u/:shortURL", (req, res) => {
+    let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+  
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
@@ -34,8 +39,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
 
